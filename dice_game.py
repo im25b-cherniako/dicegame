207388f7e3c398adf05ce716ba222a4830107460
OleksandrCherniak <oleksandrcherniak2@gmail.com>
2025-11-27 15:13:07 +0100

    start

--- dice_game.py
+++ dice_game.py
@@ -5,9 +5,19 @@ Main function
 """
 def main():
     """
-
-    :return:
+    Main func
+    :return: None
     """
     pass
 
-
+def inputs():
+    """
+    Function with programm inputs
+    :return: number of players
+    """
+    while True:
+        player_number = int(input('Geben Sie Anzahl Spieler ein (1-4) > '))
+        if 4 >= player_number >= 1:
+            return player_number
+        else:
+            continue
